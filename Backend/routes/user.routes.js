5bd458536eaf0547351e3b2319f7f623e5278e56
iammradarsh <mr1223adarsh@gmail.com>
2024-12-04 17:57:17 +0530

    user login done

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -12,4 +12,12 @@ router.post('/register',[
 userController.registerUser
 );
 
+router.post('/login',[
+    body('email').isEmail().withMessage('Inavalid email'),
+    body('password').isLength({min:6}).withMessage('Password must be at least 6 characters')
+],
+
+userController.loginUser
+);
+
 module.exports = router;
