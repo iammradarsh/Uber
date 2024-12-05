869c2492a2ecbc1a58ab548434564761385cf9cc
iammradarsh <mr1223adarsh@gmail.com>
2024-12-05 16:15:45 +0530

    user authentication done

---
+++ Backend/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+const userModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const blacklistTokenModel = require("../models/blacklistToken.model");
+
+module.exports.authUser = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  // console.log(token + "aniket");
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const isBlacklisted = await blacklistTokenModel.findOne({ token: token });
+  if (isBlacklisted) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  // console.log(process.env.JWT_SECRET + "adarsh");
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // console.log(decoded);
+    const user = await userModel.findById(decoded._id);
+    // console.log(user);
+
+    req.user = user;
+
+    return next();
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
