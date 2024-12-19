62fa270bbc0f31bece91e7c00f6b51184c5f8f78
iammradarsh <mr1223adarsh@gmail.com>
2024-12-19 17:38:17 +0530

    home page ui creating

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,82 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [pickup, setPickup] = useState("");
+  const [destination, setDestination] = useState("");
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  useGSAP(() => {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: "70%",
+      });
+    } else {
+      gsap.to(panelRef.current, {
+        height: "0%",
+      });
+    }
+  }, [panelOpen]);
+
+  return (
+    <div className="relative">
+      <img
+        className="w-16 absolute left-5 top-5"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/800px-Uber_logo_2018.svg.png"
+        alt=""
+      />
+      <div className="h-screen w-full">
+        <img
+          className="h-full w-full object-cover"
+          src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*gwMx05pqII5hbfmX.gif"
+          alt="map"
+        />
+      </div>
+      <div className="flex flex-col justify-end bg-white h-screen absolute top-0 w-full ">
+        <div className="h-[30%] p-6 bg-white">
+          <h4 className="text-3xl font-bold">Find a trip</h4>
+          <form
+            className="relative py-3"
+            onSubmit={(e) => {
+              submitHandler(e);
+            }}
+          >
+            <div className="line absolute h-16 w-1 top-[55%] -translate-y-1/2 left-5 bg-gray-700 rounded-full"></div>
+            <input
+              value={pickup}
+              onClick={() => {
+                setPanelOpen(!panelOpen);
+              }}
+              onChange={(e) => {
+                setPickup(e.target.value);
+              }}
+              className=" bg-[#eee] px-8 py-2 text-base rounded-lg w-full mt-5"
+              type="text"
+              placeholder="Add a Pickup Location"
+            />
+            <input
+              value={destination}
+              onClick={() => {
+                setPanelOpen(!panelOpen);
+              }}
+              onChange={(e) => {
+                setDestination(e.target.value);
+              }}
+              className=" bg-[#eee] px-8 py-2 text-base rounded-lg w-full mt-3"
+              type="text"
+              placeholder="Enter Your Destination"
+            />
+          </form>
+        </div>
+        <div ref={panelRef} className="bg-red-500 h-0"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
